f0b25d9d8432f6a88be83d497ef9ce30ce70779b
macario117 <36415489+macario117@users.noreply.github.com>
2018-09-09 09:09:48 -0500

    Update SMCEOToken.sol

--- CEOtoken/SMCEOToken.sol
+++ CEOtoken/SMCEOToken.sol
@@ -116,7 +116,7 @@ contract CEOTOKEN is ERC20Interface, Owned, SafeMath {
         symbol = "CEO";
         name = "CEO TOKEN";
         decimals = 18;
-        _totalSupply = 100000000000000000000000000;
+        _totalSupply = 100000000;
         balances[0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222] = _totalSupply;
         Transfer(address(0), 0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222, _totalSupply);
     }
